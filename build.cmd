fb8141abdaaec79f549bfcd79eb41536865ec21c
Jason Gardella <jason.a.gardella@gmail.com>
2020-03-05 13:34:38 -0500

    Update to netcoreapp3.1 and netstandard2.1

--- build.cmd
+++ build.cmd
@@ -1,6 +1,8 @@
 @echo off
 cls
 
+dotent tool restore
+dotent paket restore
 dotnet restore build.proj --verbosity n
 
 dotnet fake run build.fsx
--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -eu
 set -o pipefail
 
+dotent tool restore
+dotent paket restore
 dotnet restore build.proj --verbosity n
 
 dotnet fake run build.fsx
